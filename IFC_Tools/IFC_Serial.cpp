7908cdf9c6eacb24265d8b52514591ddc0f487bc
PB2 <ltbergeron@gmail.com>
2019-01-24 21:41:31 -0500

    Update IFC_Serial.cpp

--- IFC_Tools/IFC_Serial.cpp
+++ IFC_Tools/IFC_Serial.cpp
@@ -189,18 +189,54 @@ void serialEvent()
 {
 	while (IFC_COMMAND_PORT.available())
 	{
-		// get the new byte:
-		char inChar = (char)IFC_COMMAND_PORT.read();
-
-		// add it to the inputString:
+		//add it to the inputString if the packet isn't complete yet:
 		if (!myRadio.stringComplete_Radio)
+		{
+			//get the new byte:
+			char inChar = (char)IFC_COMMAND_PORT.read();
+
+			//add char to input string buffer
 			myRadio.inputString_Radio += inChar;
 
-		// if the incoming character is a newline, set a flag so the main loop can
-		// do something about it:
-		if (inChar == END_BYTE)
+			//test if the length of the string is at least one
+			if (myRadio.inputString_Radio.length() >= 1)
+			{
+				//if the first byte isn't correct, clear the string buffer and start over
+				if (myRadio.inputString_Radio[0] != START_BYTE)
+				{
+					//clear the string buffer
+					myRadio.inputString_Radio = "";
+				}
+			}
+		}
+		else
 		{
-			myRadio.stringComplete_Radio = true;
+			//not time to read from buffer - need to finish processing the completed packet contained in inputString_Radio first
+			//break the loop - otherwise "while (IFC_COMMAND_PORT.available())" could be an infinite blocking loop if bytes aren't being read from the port
+			break;
+		}
+
+		//check size of input buffer to see if it is big enough to fit a full packet
+		if (myRadio.inputString_Radio.length() >= BUFF_LEN)
+		{
+			//check to see if the char in the (BUFF_LEN - 1) position is correctly the value of END_BYTE
+			if (myRadio.inputString_Radio[BUFF_LEN - 1] == END_BYTE)
+			{
+				//optional debugging prints
+				/*for (byte i = 0; i < myRadio.inputString_Radio.length(); i++)
+				{
+					Serial.print((int)myRadio.inputString_Radio[i]); Serial.print(" ");
+				}
+				Serial.println();*/
+
+				//set this flag to true so the data can be processed
+				myRadio.stringComplete_Radio = true;
+			}
+			else
+			{
+				//clear the string buffer and start over
+				myRadio.inputString_Radio = "";
+			}
 		}
 	}
 
@@ -213,18 +249,54 @@ void serialEvent1()
 {
 	while (IFC_COMMAND_PORT.available())
 	{
-		// get the new byte:
-		char inChar = (char)IFC_COMMAND_PORT.read();
-
-		// add it to the inputString:
+		//add it to the inputString if the packet isn't complete yet:
 		if (!myRadio.stringComplete_Radio)
+		{
+			//get the new byte:
+			char inChar = (char)IFC_COMMAND_PORT.read();
+
+			//add char to input string buffer
 			myRadio.inputString_Radio += inChar;
 
-		// if the incoming character is a newline, set a flag so the main loop can
-		// do something about it:
-		if (inChar == END_BYTE)
+			//test if the length of the string is at least one
+			if (myRadio.inputString_Radio.length() >= 1)
+			{
+				//if the first byte isn't correct, clear the string buffer and start over
+				if (myRadio.inputString_Radio[0] != START_BYTE)
+				{
+					//clear the string buffer
+					myRadio.inputString_Radio = "";
+				}
+			}
+		}
+		else
 		{
-			myRadio.stringComplete_Radio = true;
+			//not time to read from buffer - need to finish processing the completed packet contained in inputString_Radio first
+			//break the loop - otherwise "while (IFC_COMMAND_PORT.available())" could be an infinite blocking loop if bytes aren't being read from the port
+			break;
+		}
+
+		//check size of input buffer to see if it is big enough to fit a full packet
+		if (myRadio.inputString_Radio.length() >= BUFF_LEN)
+		{
+			//check to see if the char in the (BUFF_LEN - 1) position is correctly the value of END_BYTE
+			if (myRadio.inputString_Radio[BUFF_LEN - 1] == END_BYTE)
+			{
+				//optional debugging prints
+				/*for (byte i = 0; i < myRadio.inputString_Radio.length(); i++)
+				{
+					Serial.print((int)myRadio.inputString_Radio[i]); Serial.print(" ");
+				}
+				Serial.println();*/
+
+				//set this flag to true so the data can be processed
+				myRadio.stringComplete_Radio = true;
+			}
+			else
+			{
+				//clear the string buffer and start over
+				myRadio.inputString_Radio = "";
+			}
 		}
 	}
 
@@ -237,18 +309,54 @@ void serialEvent2()
 {
 	while (IFC_COMMAND_PORT.available())
 	{
-		// get the new byte:
-		char inChar = (char)IFC_COMMAND_PORT.read();
-
-		// add it to the inputString:
+		//add it to the inputString if the packet isn't complete yet:
 		if (!myRadio.stringComplete_Radio)
+		{
+			//get the new byte:
+			char inChar = (char)IFC_COMMAND_PORT.read();
+
+			//add char to input string buffer
 			myRadio.inputString_Radio += inChar;
 
-		// if the incoming character is a newline, set a flag so the main loop can
-		// do something about it:
-		if (inChar == END_BYTE)
+			//test if the length of the string is at least one
+			if (myRadio.inputString_Radio.length() >= 1)
+			{
+				//if the first byte isn't correct, clear the string buffer and start over
+				if (myRadio.inputString_Radio[0] != START_BYTE)
+				{
+					//clear the string buffer
+					myRadio.inputString_Radio = "";
+				}
+			}
+		}
+		else
 		{
-			myRadio.stringComplete_Radio = true;
+			//not time to read from buffer - need to finish processing the completed packet contained in inputString_Radio first
+			//break the loop - otherwise "while (IFC_COMMAND_PORT.available())" could be an infinite blocking loop if bytes aren't being read from the port
+			break;
+		}
+
+		//check size of input buffer to see if it is big enough to fit a full packet
+		if (myRadio.inputString_Radio.length() >= BUFF_LEN)
+		{
+			//check to see if the char in the (BUFF_LEN - 1) position is correctly the value of END_BYTE
+			if (myRadio.inputString_Radio[BUFF_LEN - 1] == END_BYTE)
+			{
+				//optional debugging prints
+				/*for (byte i = 0; i < myRadio.inputString_Radio.length(); i++)
+				{
+					Serial.print((int)myRadio.inputString_Radio[i]); Serial.print(" ");
+				}
+				Serial.println();*/
+
+				//set this flag to true so the data can be processed
+				myRadio.stringComplete_Radio = true;
+			}
+			else
+			{
+				//clear the string buffer and start over
+				myRadio.inputString_Radio = "";
+			}
 		}
 	}
 
@@ -261,18 +369,54 @@ void serialEvent3()
 {
 	while (IFC_COMMAND_PORT.available())
 	{
-		// get the new byte:
-		char inChar = (char)IFC_COMMAND_PORT.read();
-
-		// add it to the inputString:
+		//add it to the inputString if the packet isn't complete yet:
 		if (!myRadio.stringComplete_Radio)
+		{
+			//get the new byte:
+			char inChar = (char)IFC_COMMAND_PORT.read();
+
+			//add char to input string buffer
 			myRadio.inputString_Radio += inChar;
 
-		// if the incoming character is a newline, set a flag so the main loop can
-		// do something about it:
-		if (inChar == END_BYTE)
+			//test if the length of the string is at least one
+			if (myRadio.inputString_Radio.length() >= 1)
+			{
+				//if the first byte isn't correct, clear the string buffer and start over
+				if (myRadio.inputString_Radio[0] != START_BYTE)
+				{
+					//clear the string buffer
+					myRadio.inputString_Radio = "";
+				}
+			}
+		}
+		else
 		{
-			myRadio.stringComplete_Radio = true;
+			//not time to read from buffer - need to finish processing the completed packet contained in inputString_Radio first
+			//break the loop - otherwise "while (IFC_COMMAND_PORT.available())" could be an infinite blocking loop if bytes aren't being read from the port
+			break;
+		}
+
+		//check size of input buffer to see if it is big enough to fit a full packet
+		if (myRadio.inputString_Radio.length() >= BUFF_LEN)
+		{
+			//check to see if the char in the (BUFF_LEN - 1) position is correctly the value of END_BYTE
+			if (myRadio.inputString_Radio[BUFF_LEN - 1] == END_BYTE)
+			{
+				//optional debugging prints
+				/*for (byte i = 0; i < myRadio.inputString_Radio.length(); i++)
+				{
+					Serial.print((int)myRadio.inputString_Radio[i]); Serial.print(" ");
+				}
+				Serial.println();*/
+
+				//set this flag to true so the data can be processed
+				myRadio.stringComplete_Radio = true;
+			}
+			else
+			{
+				//clear the string buffer and start over
+				myRadio.inputString_Radio = "";
+			}
 		}
 	}
 
@@ -285,18 +429,54 @@ void serialEvent4()
 {
 	while (IFC_COMMAND_PORT.available())
 	{
-		// get the new byte:
-		char inChar = (char)IFC_COMMAND_PORT.read();
-
-		// add it to the inputString:
+		//add it to the inputString if the packet isn't complete yet:
 		if (!myRadio.stringComplete_Radio)
+		{
+			//get the new byte:
+			char inChar = (char)IFC_COMMAND_PORT.read();
+
+			//add char to input string buffer
 			myRadio.inputString_Radio += inChar;
 
-		// if the incoming character is a newline, set a flag so the main loop can
-		// do something about it:
-		if (inChar == END_BYTE)
+			//test if the length of the string is at least one
+			if (myRadio.inputString_Radio.length() >= 1)
+			{
+				//if the first byte isn't correct, clear the string buffer and start over
+				if (myRadio.inputString_Radio[0] != START_BYTE)
+				{
+					//clear the string buffer
+					myRadio.inputString_Radio = "";
+				}
+			}
+		}
+		else
 		{
-			myRadio.stringComplete_Radio = true;
+			//not time to read from buffer - need to finish processing the completed packet contained in inputString_Radio first
+			//break the loop - otherwise "while (IFC_COMMAND_PORT.available())" could be an infinite blocking loop if bytes aren't being read from the port
+			break;
+		}
+
+		//check size of input buffer to see if it is big enough to fit a full packet
+		if (myRadio.inputString_Radio.length() >= BUFF_LEN)
+		{
+			//check to see if the char in the (BUFF_LEN - 1) position is correctly the value of END_BYTE
+			if (myRadio.inputString_Radio[BUFF_LEN - 1] == END_BYTE)
+			{
+				//optional debugging prints
+				/*for (byte i = 0; i < myRadio.inputString_Radio.length(); i++)
+				{
+					Serial.print((int)myRadio.inputString_Radio[i]); Serial.print(" ");
+				}
+				Serial.println();*/
+
+				//set this flag to true so the data can be processed
+				myRadio.stringComplete_Radio = true;
+			}
+			else
+			{
+				//clear the string buffer and start over
+				myRadio.inputString_Radio = "";
+			}
 		}
 	}
 
@@ -309,18 +489,54 @@ void serialEvent5()
 {
 	while (IFC_COMMAND_PORT.available())
 	{
-		// get the new byte:
-		char inChar = (char)IFC_COMMAND_PORT.read();
-
-		// add it to the inputString:
+		//add it to the inputString if the packet isn't complete yet:
 		if (!myRadio.stringComplete_Radio)
+		{
+			//get the new byte:
+			char inChar = (char)IFC_COMMAND_PORT.read();
+
+			//add char to input string buffer
 			myRadio.inputString_Radio += inChar;
 
-		// if the incoming character is a newline, set a flag so the main loop can
-		// do something about it:
-		if (inChar == END_BYTE)
+			//test if the length of the string is at least one
+			if (myRadio.inputString_Radio.length() >= 1)
+			{
+				//if the first byte isn't correct, clear the string buffer and start over
+				if (myRadio.inputString_Radio[0] != START_BYTE)
+				{
+					//clear the string buffer
+					myRadio.inputString_Radio = "";
+				}
+			}
+		}
+		else
 		{
-			myRadio.stringComplete_Radio = true;
+			//not time to read from buffer - need to finish processing the completed packet contained in inputString_Radio first
+			//break the loop - otherwise "while (IFC_COMMAND_PORT.available())" could be an infinite blocking loop if bytes aren't being read from the port
+			break;
+		}
+
+		//check size of input buffer to see if it is big enough to fit a full packet
+		if (myRadio.inputString_Radio.length() >= BUFF_LEN)
+		{
+			//check to see if the char in the (BUFF_LEN - 1) position is correctly the value of END_BYTE
+			if (myRadio.inputString_Radio[BUFF_LEN - 1] == END_BYTE)
+			{
+				//optional debugging prints
+				/*for (byte i = 0; i < myRadio.inputString_Radio.length(); i++)
+				{
+					Serial.print((int)myRadio.inputString_Radio[i]); Serial.print(" ");
+				}
+				Serial.println();*/
+
+				//set this flag to true so the data can be processed
+				myRadio.stringComplete_Radio = true;
+			}
+			else
+			{
+				//clear the string buffer and start over
+				myRadio.inputString_Radio = "";
+			}
 		}
 	}
 
@@ -333,18 +549,54 @@ void serialEvent6()
 {
 	while (IFC_COMMAND_PORT.available())
 	{
-		// get the new byte:
-		char inChar = (char)IFC_COMMAND_PORT.read();
-
-		// add it to the inputString:
+		//add it to the inputString if the packet isn't complete yet:
 		if (!myRadio.stringComplete_Radio)
+		{
+			//get the new byte:
+			char inChar = (char)IFC_COMMAND_PORT.read();
+
+			//add char to input string buffer
 			myRadio.inputString_Radio += inChar;
 
-		// if the incoming character is a newline, set a flag so the main loop can
-		// do something about it:
-		if (inChar == END_BYTE)
+			//test if the length of the string is at least one
+			if (myRadio.inputString_Radio.length() >= 1)
+			{
+				//if the first byte isn't correct, clear the string buffer and start over
+				if (myRadio.inputString_Radio[0] != START_BYTE)
+				{
+					//clear the string buffer
+					myRadio.inputString_Radio = "";
+				}
+			}
+		}
+		else
 		{
-			myRadio.stringComplete_Radio = true;
+			//not time to read from buffer - need to finish processing the completed packet contained in inputString_Radio first
+			//break the loop - otherwise "while (IFC_COMMAND_PORT.available())" could be an infinite blocking loop if bytes aren't being read from the port
+			break;
+		}
+
+		//check size of input buffer to see if it is big enough to fit a full packet
+		if (myRadio.inputString_Radio.length() >= BUFF_LEN)
+		{
+			//check to see if the char in the (BUFF_LEN - 1) position is correctly the value of END_BYTE
+			if (myRadio.inputString_Radio[BUFF_LEN - 1] == END_BYTE)
+			{
+				//optional debugging prints
+				/*for (byte i = 0; i < myRadio.inputString_Radio.length(); i++)
+				{
+					Serial.print((int)myRadio.inputString_Radio[i]); Serial.print(" ");
+				}
+				Serial.println();*/
+
+				//set this flag to true so the data can be processed
+				myRadio.stringComplete_Radio = true;
+			}
+			else
+			{
+				//clear the string buffer and start over
+				myRadio.inputString_Radio = "";
+			}
 		}
 	}
 
