7b30cc7a49c1c0222990c537e3be505f0e94fa39
PB2 <ltbergeron@gmail.com>
2019-02-10 22:46:10 -0500

    Update GS_Tools.cpp

--- GS_Tools/GS_Tools.cpp
+++ GS_Tools/GS_Tools.cpp
@@ -22,7 +22,7 @@ void GS_Class::begin()
 
 
 	//wait for all serial ports to come online
-	//while (!DEBUG_PORT);
+	//while (!GS_DEBUG_PORT);
 	GS_DEBUG_PORT.println(F("Initializing serial ports..."));
 	GS_DEBUG_PORT.print(F("Initializing command port at Serial")); GS_DEBUG_PORT.print(GS_COMMAND_PORT_NUMBER);  GS_DEBUG_PORT.println(F("..."));
 	GS_DEBUG_PORT.print(F("Initializing telemetry at Serial")); GS_DEBUG_PORT.print(GS_TELEM_PORT_NUMBER);  GS_DEBUG_PORT.println(F("..."));
@@ -65,6 +65,18 @@ void GS_Class::begin()
 	myRadio.begin(false);
 	GS_DEBUG_PORT.println(F("\tAirComms initialized"));
 
+
+
+
+	//initialize "pass-through" timers
+	GS_DEBUG_PORT.println(F("Initializing timers..."));
+	timeBench_Commands = millis();
+	currentTime_Commands = timeBench_Commands;
+
+	timeBench_Telem = millis();
+	currentTime_Telem = timeBench_Telem;
+	GS_DEBUG_PORT.println(F("\tTimers initialized..."));
+
 	return;
 }
 
@@ -73,8 +85,15 @@ void GS_Class::begin()
 
 int GS_Class::grabData_Radio()
 {
-	int result = myRadio.grabData_Radio();
+	int result;
 
+	//check to see if there is a loss of radio link between GS and IFC
+	checkRadioLink();
+	
+	//see if new data is available
+	result = myRadio.grabData_Radio();
+
+	//save new data if available
 	if (result == AIR_NEW_DATA)
 	{
 		telemetry.altitude = myRadio.incomingArray[AIR_ALTITUDE_INDEX] / 100.0;
@@ -83,9 +102,60 @@ int GS_Class::grabData_Radio()
 		telemetry.velocity = myRadio.incomingArray[AIR_PITOT_INDEX] / 100.0;
 		telemetry.latitude = myRadio.incomingArray[AIR_LATITUDE_INDEX] / 100.0;
 		telemetry.longitude = myRadio.incomingArray[AIR_LONGITUDE_INDEX] / 100.0;
+		telemetry.UTC_year = myRadio.incomingArray[AIR_UTC_YEAR_INDEX];
+		telemetry.UTC_month = myRadio.incomingArray[AIR_UTC_MONTH_INDEX];
+		telemetry.UTC_day = myRadio.incomingArray[AIR_UTC_DAY_INDEX];
+		telemetry.UTC_hour = myRadio.incomingArray[AIR_UTC_HOUR_INDEX];
+		telemetry.UTC_minute = myRadio.incomingArray[AIR_UTC_MINUTE_INDEX];
+		telemetry.UTC_second = myRadio.incomingArray[AIR_UTC_SECOND_INDEX];
+		telemetry.speedOverGround = myRadio.incomingArray[AIR_SOG_INDEX] / 100.0;
+		telemetry.courseOverGround = myRadio.incomingArray[AIR_COG_INDEX] / 100.0;
 	}
 
+	//return boolean to tell main program if there is new data available
 	return result;
+}
+
+
+
+
+//send data to datalogging computer via debugging port
+void GS_Class::sendTelem()
+{
+	GS_DEBUG_PORT.println("New Telem:");
+	GS_DEBUG_PORT.print("Alt: ");	GS_DEBUG_PORT.println(telemetry.altitude, 5);
+	GS_DEBUG_PORT.print("Roll: ");	GS_DEBUG_PORT.println(telemetry.rollAngle, 5);
+	GS_DEBUG_PORT.print("Pitch: ");	GS_DEBUG_PORT.println(telemetry.pitchAngle, 5);
+	GS_DEBUG_PORT.print("Vel: ");	GS_DEBUG_PORT.println(telemetry.velocity, 5);
+	GS_DEBUG_PORT.print("Lat: ");	GS_DEBUG_PORT.println(telemetry.latitude, 5);
+	GS_DEBUG_PORT.print("Lon: ");	GS_DEBUG_PORT.println(telemetry.longitude, 5);
+	GS_DEBUG_PORT.print("UTC_y: ");	GS_DEBUG_PORT.println(telemetry.UTC_year);
+	GS_DEBUG_PORT.print("UTC_M: ");	GS_DEBUG_PORT.println(telemetry.UTC_month);
+	GS_DEBUG_PORT.print("UTC_d: ");	GS_DEBUG_PORT.println(telemetry.UTC_day);
+	GS_DEBUG_PORT.print("UTC_h: ");	GS_DEBUG_PORT.println(telemetry.UTC_hour);
+	GS_DEBUG_PORT.print("UTC_m: ");	GS_DEBUG_PORT.println(telemetry.UTC_minute);
+	GS_DEBUG_PORT.print("UTC_s: ");	GS_DEBUG_PORT.println(telemetry.UTC_second);
+	GS_DEBUG_PORT.print("SOG: ");	GS_DEBUG_PORT.println(telemetry.speedOverGround, 3);
+	GS_DEBUG_PORT.print("COG: ");	GS_DEBUG_PORT.println(telemetry.courseOverGround, 3);
+	GS_DEBUG_PORT.println();
+
+	return;
+}
+
+
+
+
+//determine each command value based off GS sensor data
+void GS_Class::computeCommands()
+{
+	//read and map joystick data
+	//mapping normalized to max and min joystick values
+	controlInputs.roll_command = updateServoCommand(ailerons);
+	controlInputs.pitch_command = updateServoCommand(elevator);
+	controlInputs.yaw_command = updateServoCommand(rudder);
+	controlInputs.throttle_command = updateServoCommand(throttle);
+
+	return;
 }
 
 
@@ -104,6 +174,13 @@ void GS_Class::sendCommands()
 	myRadio.outgoingArray[AIR_LANDING_GEAR_INDEX] = myGS.controlInputs.landingGear_command;
 	myRadio.outgoingArray[AIR_FLAPS_INDEX] = myGS.controlInputs.flaps_command;*/
 
+	//optional debugging prints to see what is in the outgoing array
+	/*for (byte i = 0; i < AIR_DATA_LEN; i++)
+	{
+		Serial.println(myRadio.outgoingArray[i]);
+	}
+	Serial.println();*/
+
 	//send the telemetry data to GS
 	myRadio.sendData();
 
@@ -113,17 +190,65 @@ void GS_Class::sendCommands()
 
 
 
-//determine each command value based off GS sensor data
-void GS_Class::computeCommands()
+//determine each command value based off GS sensor data and send commands to plane
+void GS_Class::computeAndSendCommands()
 {
-	//read and map joystick data
-	//mapping normalized to max and min joystick values
-	myGS.controlInputs.roll_command = updateServoCommand(ailerons);
-	myGS.controlInputs.pitch_command = updateServoCommand(elevator);
-	myGS.controlInputs.yaw_command = updateServoCommand(rudder);
-	myGS.controlInputs.throttle_command = updateServoCommand(throttle);
+	//use timer to send commands to the plane at a fixed rate
+	currentTime_Commands = millis();
+	if ((currentTime_Commands - timeBench_Commands) >= REPORT_COMMANDS_PERIOD)
+	{
+		//reset timer
+		timeBench_Commands = currentTime_Commands;
+
+		//determine each command value based off GS sensor data
+		computeCommands();
+
+		//send commands to plane
+		sendCommands();
+	}
 
 	return;
+}
+
+
+
+
+//check to see if there is a loss of radio link between GS and IFC
+bool GS_Class::checkRadioLink()
+{
+	//see if new data has arrived - if not, test if the timeout condition is met
+	if (!myRadio.arrayComplete_Radio)
+	{
+		//timer to see if there is a loss of link between GS and plane
+		myGS.currentTime_Telem = millis();
+
+		if (!noPacketFlag)
+		{
+			myGS.timeBench_Telem = myGS.currentTime_Telem;
+			noPacketFlag = true;
+		}
+
+		if ((myGS.currentTime_Telem - myGS.timeBench_Telem) >= LOSS_LINK_TIMOUT)
+		{
+			//link is severed - unset flag if not alread unset
+			linkConnected = false;
+			GS_DEBUG_PORT.println("loss link");
+		}
+		else
+		{
+			//not enough time has passed to determine if the link is severed - set flag if not already set
+			//do not reset timer
+			linkConnected = true;
+		}
+	}
+	else
+	{
+		//new data has arrived - link is connected
+		linkConnected = true;
+		noPacketFlag = false;
+	}
+
+	return linkConnected;
 }
 
 
@@ -137,12 +262,17 @@ int16_t GS_Class::updateServoCommand(controlSurfaces controlSurface)
 	//read the analog voltage of the potentiometer
 	uint16_t analogValue = analogRead(controlSurface.analog_pin);
 
-	/*GS_DEBUG_PORT.print("Command_Index: "); GS_DEBUG_PORT.println(controlSurface.command_index);
-	GS_DEBUG_PORT.print("ADC_min: "); GS_DEBUG_PORT.println(controlSurface.ADC_min);
-	GS_DEBUG_PORT.print("Analog: "); GS_DEBUG_PORT.println(analogValue);
-	GS_DEBUG_PORT.print("ADC_max: "); GS_DEBUG_PORT.println(controlSurface.ADC_max);
-	GS_DEBUG_PORT.print("high_rates_surface_min: "); GS_DEBUG_PORT.println(controlSurface.high_rates_surface_min);
-	GS_DEBUG_PORT.println();*/
+	//optional debugging prints
+	/*if (controlSurface.command_index == AILERON_INDEX)
+	{
+		GS_DEBUG_PORT.print("Command_Index: "); GS_DEBUG_PORT.println(controlSurface.command_index);
+		GS_DEBUG_PORT.print("ADC_min: "); GS_DEBUG_PORT.println(controlSurface.ADC_min);
+		GS_DEBUG_PORT.print("Analog: "); GS_DEBUG_PORT.println(analogValue);
+		GS_DEBUG_PORT.print("ADC_max: "); GS_DEBUG_PORT.println(controlSurface.ADC_max);
+		GS_DEBUG_PORT.print("high_rates_surface_min: "); GS_DEBUG_PORT.println(controlSurface.high_rates_surface_min);
+		GS_DEBUG_PORT.println();
+	}*/
+	
 
 	//throttle is processed opposite of all other commands
 	if (controlSurface.command_index == AIR_THROTTLE_INDEX)
@@ -171,12 +301,16 @@ int16_t GS_Class::updateServoCommand(controlSurfaces controlSurface)
 			controlSurface.high_rates_surface_max	//maximum value
 		);
 
-	/*GS_DEBUG_PORT.print("Command_Index: "); GS_DEBUG_PORT.println(controlSurface.command_index);
-	GS_DEBUG_PORT.print("Value: "); GS_DEBUG_PORT.println(commandValue);
-	GS_DEBUG_PORT.print("high_rates_surface_min: "); GS_DEBUG_PORT.println(controlSurface.high_rates_surface_min);
-	GS_DEBUG_PORT.print("high_rates_surface_max: "); GS_DEBUG_PORT.println(controlSurface.high_rates_surface_max);
-	GS_DEBUG_PORT.print("_offset: "); GS_DEBUG_PORT.println(controlSurface._offset);
-	GS_DEBUG_PORT.println();*/
+	//optional debugging prints
+	/*if (controlSurface.command_index == AILERON_INDEX)
+	{
+		GS_DEBUG_PORT.print("Command_Index: "); GS_DEBUG_PORT.println(controlSurface.command_index);
+		GS_DEBUG_PORT.print("Value: "); GS_DEBUG_PORT.println(commandValue);
+		GS_DEBUG_PORT.print("high_rates_surface_min: "); GS_DEBUG_PORT.println(controlSurface.high_rates_surface_min);
+		GS_DEBUG_PORT.print("high_rates_surface_max: "); GS_DEBUG_PORT.println(controlSurface.high_rates_surface_max);
+		GS_DEBUG_PORT.print("_offset: "); GS_DEBUG_PORT.println(controlSurface._offset);
+		GS_DEBUG_PORT.println();
+	}*/
 
 	return commandValue;
 }
