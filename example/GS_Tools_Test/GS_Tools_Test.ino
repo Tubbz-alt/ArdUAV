97b03c659ba4e488c50f9cb5bcb166ff04c72c24
PB2 <ltbergeron@gmail.com>
2019-02-10 22:48:09 -0500

    Update GS_Tools_Test.ino

--- example/GS_Tools_Test/GS_Tools_Test.ino
+++ example/GS_Tools_Test/GS_Tools_Test.ino
@@ -3,18 +3,6 @@
 
 
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
-//setup user defined global variables and functions here
-//below are variables to make a "pass-through timer" allowing the plane to periodically send data
-//to the plane at a fixed rate without blocking processing - default is 10ms = 100Hz
-unsigned long timeBench = millis();
-unsigned long currentTime = timeBench;
-byte transmitPeriod = 10;
-///////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-
 void setup()
 {
   //initialize the core ground station software class
@@ -29,30 +17,13 @@ void loop()
   //get telemetry data from the plane
   if(myGS.grabData_Radio())
   {
-    //optinal debugging prints
-    /*GS_DEBUG_PORT.print("Altitude: "); GS_DEBUG_PORT.println(myGS.telemetry.altitude, 2);
-    GS_DEBUG_PORT.print("Roll Angle: "); GS_DEBUG_PORT.println(myGS.telemetry.rollAngle, 2);
-    GS_DEBUG_PORT.print("Pitch Angle: "); GS_DEBUG_PORT.println(myGS.telemetry.pitchAngle, 2);
-    GS_DEBUG_PORT.print("Velocity: "); GS_DEBUG_PORT.println(myGS.telemetry.velocity, 2);
-    GS_DEBUG_PORT.print("Latitude: "); GS_DEBUG_PORT.println(myGS.telemetry.latitude, 2);
-    GS_DEBUG_PORT.print("Longitude: "); GS_DEBUG_PORT.println(myGS.telemetry.longitude, 2);
-    GS_DEBUG_PORT.println();*/
+    //send data to datalogging computer via debugging port
+    myGS.sendTelem();
   }
 
-  //use timer to send commands to the plane at a fixed rate
-  currentTime = millis();
-  if((currentTime - timeBench)>= transmitPeriod)
-  {
-    timeBench = currentTime;
-
-    //update command values
-    myGS.computeCommands();
-
-    //send commands to the plane
-    myGS.sendCommands();
-  }
+  //determine each command value based off GS sensor data and send commands to plane
+  myGS.computeAndSendCommands();
 }
-
 
 
 
