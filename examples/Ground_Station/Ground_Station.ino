a0df9f95853c436c8cbaae30a4602a56d1f2e29d
PB2 <gitstuff2@gmail.com>
2019-10-05 22:55:04 -0400

    Update Ground_Station.ino

--- examples/Ground_Station/Ground_Station.ino
+++ examples/Ground_Station/Ground_Station.ino
@@ -13,11 +13,7 @@ void setup()
 
 
 void loop()
-{                                      // Signal   | min | mid | max | directions
-  GS_commandTransfer.txBuff[16] = 110; // FPV Pan  | 0   | 110 | 180 | left <-> right
-  GS_commandTransfer.txBuff[17] = 170; // FPV Tilt | 80  | 170 | 180 | down <-> up
-  GS_commandTransfer.txBuff[18] = 140; // Bay Door | 40  | n/a | 140 | open <-> closed
-  
+{
   //determine each command value based off GS sensor data and send commands to plane
   myGS.computeAndSendCommands();
 
